868e7634f478a89666f0c28a7f8b975e215f584f
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-04-04 18:05:38 +0800

    Cleaned up Flex properties of the Project Section

--- scripts/viewport.js
+++ scripts/viewport.js
@@ -6,3 +6,14 @@ function ViewPort()
     }
 
 ViewPort()
+
+var checkScrollBars = function(){
+    var b = $('body');
+    var normalw = 0;
+    var scrollw = 0;
+    if(b.prop('scrollHeight')>b.height()){
+        normalw = window.innerWidth;
+        scrollw = normalw - b.width();
+        $('#container').css({marginRight:'-'+scrollw+'px'});
+    }
+}
