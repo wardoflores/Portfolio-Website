a5a6173531ae398480652c0cbe6fa74ce77e52a0
Joey Flores <61069122+wardoflores@users.noreply.github.com>
2023-02-10 18:14:00 +0800

    Added Google Analytics tag

--- v2-framework/src/routes/+layout.svelte
+++ v2-framework/src/routes/+layout.svelte
@@ -4,6 +4,18 @@
     import '$lib/styles/style.css'
 </script>
 
+<svelte:head>
+<!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WQWTWT736P"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-WQWTWT736P');
+    </script>
+</svelte:head>
+
 <Header />
 
 <main>
