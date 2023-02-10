d2c2b9d25d2c540025c2a8e9dcf33d56e9325993
Joey Flores <61069122+wardoflores@users.noreply.github.com>
2023-02-11 00:57:54 +0800

    Uncommented the GA Tags of both websites

--- v2-framework/src/routes/+layout.svelte
+++ v2-framework/src/routes/+layout.svelte
@@ -6,14 +6,14 @@
 
 <svelte:head>
 <!-- Google tag (gtag.js) -->
-<!--    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WQWTWT736P"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WQWTWT736P"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-WQWTWT736P');
-    </script> -->
+    </script>
 </svelte:head>
 
 <Header />
