89b71e6b9d777d19c362d5908b7fb5d262cb2c7b
wardoflores <61069122+wardoflores@users.noreply.github.com>
2023-02-19 08:47:08 +0800

    Fixed blog slug

--- v2-framework/src/routes/blog/[slug]/+page.svelte
+++ v2-framework/src/routes/blog/[slug]/+page.svelte
@@ -1,10 +1,11 @@
 <script type="ts">
 	import { page } from "$app/stores";
-  export let data;
+  export let data
+  const { title, date, Content } = data
 </script>
 
 <article>
-  <h1>{ data.title }</h1>
-  <p>Published: {data.date}</p>
-  <svelte:component this={data.content} />
+  <h1>{title}</h1>
+  <p>Published: {date}</p>
+  <Content />
 </article>
